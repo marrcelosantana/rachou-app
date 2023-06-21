7f61de70ba33fe023debf43482718c1213b0e8d6
marrcelosantana <marcelosantana@alu.ufc.br>
2023-06-21 16:41:09 -0300

    [Rachou App] - update theme

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -7,7 +7,7 @@ export function Loading() {
 
   return (
     <Container>
-      <Spinner size="lg" color={COLORS.BLUE} />
+      <Spinner size="lg" color={COLORS.PURPLE} />
     </Container>
   );
 }
--- src/themes/default-theme.ts
+++ src/themes/default-theme.ts
@@ -1,10 +1,14 @@
 export const defaultTheme = {
   COLORS: {
     WHITE: "#FFFFFF",
-    BLUE: "#5636d3",
+    PURPLE: "#5636d3",
     ORANGE: "#FF872C",
     GREEN: "#12A454",
     RED: "#E83F5B",
+
+    LIGHT_GREEN: "rgba(18, 164, 84, 0.2)",
+    LIGHT_RED: "rgba(232, 63, 91, 0.2)",
+    LIGHT_ORANGE: "rgba(255, 135, 44, 0.2)",
 
     TITLE: "#363F5F",
     TEXT: "#969CB3",
