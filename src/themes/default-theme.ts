d106817876752b66b60b1aefc3aae74ef102bbff
marrcelosantana <marcelosantana@alu.ufc.br>
2023-07-25 20:49:55 -0300

    [Rashow] Theme colors config

--- src/themes/default-theme.ts
+++ src/themes/default-theme.ts
@@ -1,18 +1,52 @@
 export const defaultTheme = {
   COLORS: {
     WHITE: "#FFFFFF",
-    PURPLE: "#5636d3",
-    ORANGE: "#FF872C",
-    GREEN: "#12A454",
-    RED: "#E83F5B",
+    BLACK: "#000000",
 
-    LIGHT_GREEN: "rgba(18, 164, 84, 0.2)",
-    LIGHT_RED: "rgba(232, 63, 91, 0.2)",
-    LIGHT_ORANGE: "rgba(255, 135, 44, 0.2)",
+    PURPLE: "#AF52DE",
+    LIGHT_PURPLE: "#BF5AF2",
 
-    TITLE: "#363F5F",
-    TEXT: "#969CB3",
-    BACKGROUND: "#F0F2F5",
+    ORANGE: "#FF9500",
+    LIGHT_ORANGE: "#FF9F0A",
+
+    YELLOW: "#FFCC00",
+    LIGHT_YELLOW: "#FFD60A",
+
+    PINK: "#FF2D55",
+    LIGHT_PINK: "#FF375F",
+
+    BROWN: "#A2845E",
+    LIGHT_BROWN: "#AC8E68",
+
+    RED: "#FF3B30",
+    LIGHT_RED: "#FF453A",
+
+    GREEN: "#34C759",
+    LIGHT_GREEN: "#30D158",
+
+    BLUE: "#007AFF",
+    LIGHT_BLUE: "#0A84FF",
+
+    MINT: "#00C7BE",
+    LIGHT_MINT: "#63E6E2",
+
+    TEAL: "#30B0C7",
+    LIGHT_TEAL: "#40CBE0",
+
+    CYAN: "#32ADE6",
+    LIGHT_CYAN: "#64D2FF",
+
+    INDIGO: "#5856D6",
+    LIGHT_INDIGO: "#5E5CE6",
+
+    GRAY_1: "#8E8E93",
+    GRAY_2: "#AEAEB2",
+    GRAY_3: "#C7C7CC",
+    GRAY_4: "#D1D1D6",
+    GRAY_5: "#E5E5EA",
+    GRAY_6: "#F2F2F7",
+
+    BACKGROUND: "#2C2C2E",
   },
 
   FONT_FAMILY: {
