103670063813f163d4f28f2eff0386e97e552089
marrcelosantana <marcelosantana@alu.ufc.br>
2023-07-27 16:12:15 -0300

    [Rashow] Init home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,37 @@
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
-import { Button, Container, Subtitle, Title } from "./styles";
+import LogoImg from "@assets/white-logo.svg";
+import PlayerImg from "@assets/player.png";
+
+import {
+  Container,
+  Header,
+  HiddenItem,
+  Highlight,
+  HighlightImage,
+  HighlightText,
+  UserImage,
+} from "./styles";
 
 export function Home() {
   const navigator = useNavigation<AppNavigatorRoutesProps>();
 
   return (
     <Container>
-      <Title>Home Page</Title>
-      <Button onPress={() => navigator.goBack()}>
-        <Subtitle>Go Back</Subtitle>
-      </Button>
+      <Header>
+        <HiddenItem />
+        <LogoImg />
+        <UserImage source={{ uri: "https://github.com/marrcelosantana.png" }} />
+      </Header>
+
+      <Highlight>
+        <HighlightText>
+          Gerencie seus{"\n"}rachas com{"\n"}facilidade e jogue{"\n"}com paixão!
+        </HighlightText>
+
+        <HighlightImage source={PlayerImg} />
+      </Highlight>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,10 +1,52 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+  padding: 80px 24px 0 24px;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const HiddenItem = styled.View``;
+
+export const UserImage = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 9999px;
+`;
+
+export const Highlight = styled.View`
+  margin-top: 32px;
+  background-color: ${({ theme }) => theme.COLORS.BLUE};
+  border-radius: 15px;
+  height: 171px;
+  padding: 0 16px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const HighlightText = styled.Text`
+  font-size: ${RFValue(17.5)}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  line-height: 24px;
+  max-width: 100%;
+  margin-top: 30px;
+`;
+
+export const HighlightImage = styled.Image`
+  width: ${RFValue(180)}px;
+  height: ${RFValue(170)}px;
+  bottom: 0;
+  right: 0;
+  position: absolute;
 `;
 
 export const Title = styled.Text`
